4f9eb9dff6dc32628df8c1c0abf1d95a9cea7f41
Paul Abboud <abboudpaul112@gmail.com>
2023-03-03 22:57:59 -0800

    finish lab02

--- lab02/IntList/SquarePrimesTest.java
+++ lab02/IntList/SquarePrimesTest.java
@@ -17,4 +17,26 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesZero() {
+        IntList lst = IntList.of(0);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesComposite() {
+        IntList lst = IntList.of(4, 15, 16, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 15 -> 16 -> 18", lst.toString());
+        assertFalse(changed);
+    }
+    @Test
+    public void testSquarePrimesFirst() {
+        IntList lst = IntList.of(2,3,5,7,11,13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121 -> 169", lst.toString());
+        assertTrue(changed);
+    }
 }
